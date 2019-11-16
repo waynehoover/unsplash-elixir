3baf3bedfd5153de4b7edeec81146ac9841afb4f
Wayne Hoover <wayne.hoover@legitscript.com>
2019-11-16 14:44:33 -0800

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Unsplash.Mixfile do
   def project do
     [
       app: :unsplash,
-      version: "1.1.0",
+      version: "1.2.0",
       elixir: "~> 1.6",
       description: "Unsplash API for Elixir",
       source_url: "https://github.com/waynehoover/unsplash-elixir",
